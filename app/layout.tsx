5059cf10f008b572761da675d899dcbd4ffe51e6
Huzaifa Naseer <huzaifanaseerkhan02@gmail.com>
2023-12-24 18:11:14 +0500

    Fixed layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import React from "react";
 import { ClerkProvider } from "@clerk/nextjs";
 import { Inter, Space_Grotesk } from "next/font/google";
@@ -7,13 +8,13 @@ import { ThemeProvider } from "@/context/ThemeProvider";
 
 const inter = Inter({
   subsets: ["latin"],
-  weights: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
   variable: "--font-inter",
 });
 
 const spaceGrotesk = Space_Grotesk({
   subsets: ["latin"],
-  weights: [300, 400, 500, 600, 700],
+  weight: ["300", "400", "500", "600", "700"],
   variable: "--font-spaceGrotesk",
 });
 
