ce08b9c7cb250fcd2361a157bed66231bd342d38
Huzaifa Naseer <huzaifanaseerkhan02@gmail.com>
2023-12-24 18:37:21 +0500

    Changed a comment

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -75,7 +75,7 @@ export async function POST(req: Request) {
     const { id, email_addresses, image_url, username, first_name, last_name } =
       evt.data;
 
-    // create a new user in your database
+    // create new user in your database
     const mongoUser = await updateUser({
       clerkId: id,
       updateData: {
