70c636cdbb022a8e4921ea1818f750b62fd1682b
Huzaifa Naseer <huzaifanaseerkhan02@gmail.com>
2023-12-31 20:08:13 +0500

    Question Answers and Voting

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -5,12 +5,14 @@ import { connectToDatabase } from "../mongoose";
 import {
   CreateUserParams,
   DeleteUserParams,
+  GetAllUsersParams,
+  GetUserByIdParams,
   UpdateUserParams,
 } from "./shared.types";
 import { revalidatePath } from "next/cache";
 import Question from "@/database/question.model";
 
-export async function getUserById(params: any) {
+export async function getUserById(params: GetUserByIdParams) {
   try {
     connectToDatabase();
 
@@ -86,3 +88,29 @@ export async function deleteUser(params: DeleteUserParams) {
     throw error;
   }
 }
+
+export async function getAllUsers(params: GetAllUsersParams) {
+  try {
+    connectToDatabase();
+
+    // const { page = 1, pageSize = 20, filter, searchQuery } = params;
+
+    const users = await User.find({}).sort({ createdAt: -1 });
+
+    return { users };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+// export async function getAllUsers(params: GetAllUsersParams){
+//   try {
+//     connectToDatabase();
+//   }
+
+//   catch (error) {
+//     console.log(error);
+//     throw error;
+//   }
+// }
